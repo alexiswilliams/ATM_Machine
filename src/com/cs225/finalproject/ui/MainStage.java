e78e7192df18e02dd7b02f2d9757c5ce66125ebe
willip27 <pariswilliams@Pariss-MacBook-Pro.local>
2017-06-19 02:34:34 -0400

    Not sure if this is working

--- src/com/cs225/finalproject/ui/MainStage.java
+++ src/com/cs225/finalproject/ui/MainStage.java
@@ -1,6 +1,7 @@
 package com.cs225.finalproject.ui;
 
 import com.cs225.finalproject.utils.Constants;
+import com.sun.corba.se.impl.orbutil.closure.Constant;
 import com.sun.java.swing.plaf.windows.resources.windows;
 
 import javafx.application.Application;
@@ -41,7 +42,159 @@ public class MainStage extends Application {
 	private TextField accountNumberInput, accountPinInput;
 
 	public MainStage() {
-		buildScene(Constants.LOGIN_LABEL);
+		//		buildScene(Constants.LOGIN_LABEL);
+	}
+
+	private Scene getLoginScene() {
+		loginButton = new Button(Constants.LOGIN_LABEL);
+		createAccountButton = new Button(Constants.CREATE_NEW_ACCOUNT_LABEL);
+		grid = new GridPane();
+		// Create labels for login scene
+		accountNumberLabel = new Label(Constants.ACCOUNT_NUMBER_LABEL);
+		accountPinLabel = new Label(Constants.ACCOUNT_PIN_LABEL);
+
+		// Create inputs for login scene
+		accountNumberInput = new TextField();
+		accountPinInput = new TextField();
+		accountNumberInput.setPromptText(Constants.ACCOUNT_NUMBER_LABEL);
+		accountPinInput.setPromptText(Constants.ACCOUNT_PIN_LABEL);
+
+		grid.setPadding(new Insets(10,10,10,10));
+		// set spacing between nodes
+		grid.setVgap(8);
+		grid.setHgap(10);
+
+		// assign nodes to GridPane
+		GridPane.setConstraints(accountNumberLabel, 0, 0);
+		GridPane.setConstraints(accountNumberInput, 1, 0);
+		GridPane.setConstraints(accountPinLabel, 0, 1);
+		GridPane.setConstraints(accountPinInput, 1, 1);
+		GridPane.setConstraints(loginButton, 0, 2);
+		GridPane.setConstraints(createAccountButton, 1, 2);
+		grid.getChildren().addAll(
+				accountNumberLabel, accountNumberInput, accountPinLabel, accountPinInput, loginButton, createAccountButton);
+
+		// event handler
+		loginButton.setOnAction(e -> {
+			buildScene(Constants.DEPOSIT_LABEL);
+			updateTopBorder(menuBar);
+		});
+		BorderPane loginBorderPane = new BorderPane();
+		loginBorderPane.setTop(getMenuBar(Constants.LOGIN_LABEL));
+		loginBorderPane.setCenter(grid);
+		Scene loginScene = new Scene(loginBorderPane);
+		return loginScene;
+	}
+
+	private Scene getDepositScene() {
+
+		return null;
+	}
+
+	private Scene getWithdrawalScene() {
+
+		return null;
+	}
+
+	private Scene getTransferFundsScene() {
+
+		return null;
+	}
+
+	private Scene getMainScene() {
+
+		return null;
+	}
+
+	private Scene getAdditionalOptionsScene() {
+
+		return null;
+	}
+
+	private Scene getCurrentBalanceScene() {
+
+		return null;
+	}
+
+	private Scene getFaqScene() {
+
+		return null;
+	}
+
+	private Scene getChangePinScene() {
+
+		return null;
+	}
+
+	private MenuBar getMenuBarLoginScreen() {
+		// Create MenuBar
+		MenuBar menuBarLoginScreen = new MenuBar();
+		// Create Menus
+		fileMenu = new Menu(Constants.FILE_LABEL);
+		optionsMenu = new Menu(Constants.OPTIONS_LABEL);
+		helpMenu = new Menu(Constants.HELP_LABEL);
+		// create Menu Items
+		exitMenuItem = new MenuItem(Constants.EXIT_LABEL);
+		contactUsMenuItem = new MenuItem(Constants.CONTACT_US_LABEL);
+		faqMenuItem = new MenuItem(Constants.FAQ_LABEL);
+
+		fileMenu.getItems().add(exitMenuItem);
+		helpMenu.getItems().addAll(contactUsMenuItem, faqMenuItem);
+
+		// Add menus to menuBar
+		menuBar.getMenus().addAll(fileMenu, optionsMenu, helpMenu);
+
+		// disable close button
+		exitMenuItem.setOnAction(e -> {
+			Platform.exit();
+			// TODO add call to generate receipt
+		});
+
+		return menuBarLoginScreen;
+	}
+
+	private MenuBar getMenuBar(String sceneType) {
+		// Create MenuBar
+		menuBar = new MenuBar();
+		// Create Menus
+		fileMenu = new Menu(Constants.FILE_LABEL);
+		optionsMenu = new Menu(Constants.OPTIONS_LABEL);
+		helpMenu = new Menu(Constants.HELP_LABEL);
+		// create Menu Items
+		exitMenuItem = new MenuItem(Constants.EXIT_LABEL);
+		contactUsMenuItem = new MenuItem(Constants.CONTACT_US_LABEL);
+		faqMenuItem = new MenuItem(Constants.FAQ_LABEL);
+		System.out.println(sceneType + " " + sceneType.equals(Constants.LOGIN_LABEL));
+		//		if(!sceneType.equals(Constants.LOGIN_LABEL)) {
+		depositMenuItem = new MenuItem(Constants.DEPOSIT_LABEL);
+		withdrawalMenuItem = new MenuItem(Constants.WITHDRAWAL_LABEL);
+		transferFundsMenuItem = new MenuItem(Constants.TRANSFER_FUNDS_LABEL);
+		getBalanceMenuItem = new MenuItem(Constants.GET_BALANCE_LABEL);
+		logoutMenuItem = new MenuItem(Constants.LOGOUT_LABEL);
+		changePinMenuItem = new MenuItem(Constants.CHANGE_PIN_LABEL);
+		createNewAccountMenuItem = new MenuItem(Constants.CREATE_NEW_ACCOUNT_LABEL);
+		viewTransactionHistoryMenuItem = new MenuItem(Constants.VIEW_TRANSACTION_HISTORY_LABEL);
+
+		// Add menu items to respective menus
+		fileMenu.getItems().addAll(depositMenuItem, withdrawalMenuItem, transferFundsMenuItem, getBalanceMenuItem, exitMenuItem);
+		optionsMenu.getItems().addAll(logoutMenuItem, changePinMenuItem, createNewAccountMenuItem, viewTransactionHistoryMenuItem);
+		helpMenu.getItems().addAll(contactUsMenuItem, faqMenuItem);
+		//		} 
+		//		else {
+		//			fileMenu.getItems().add(exitMenuItem);
+		//			helpMenu.getItems().addAll(contactUsMenuItem, faqMenuItem);
+		//		}
+
+		// Add menus to menuBar
+		menuBar.getMenus().addAll(fileMenu, optionsMenu, helpMenu);
+
+		// disable close button
+		exitMenuItem.setOnAction(e -> {
+			Platform.exit();
+			// TODO add call to generate receipt
+		});
+
+		return menuBar;
 	}
 
 	private void updateMenuBar(String sceneType) {
@@ -87,8 +240,11 @@ public class MainStage extends Application {
 	private void buildScene(String sceneType) { 
 		updateMenuBar(sceneType);
 
-		if(!sceneType.equals(Constants.LOGIN_LABEL)) {
-		} else {
+		if(sceneType.equals(Constants.DEPOSIT_LABEL)) {
+
+		} else if(sceneType.equals(Constants.WITHDRAWAL_LABEL)) {
+
+		} else if (sceneType.equals(Constants.LOGIN_LABEL)){
 			loginButton = new Button(Constants.LOGIN_LABEL);
 			createAccountButton = new Button(Constants.CREATE_NEW_ACCOUNT_LABEL);
 			grid = new GridPane();
@@ -134,10 +290,7 @@ public class MainStage extends Application {
 		mainStage.setResizable(false);
 		mainStage.setOnCloseRequest(e -> e.consume());
 
-		borderPane = new BorderPane();
-		updateTopBorder(menuBar);
-		borderPane.setCenter(grid);
-		Scene scene = new Scene(borderPane);
+		Scene scene = getLoginScene();
 		// Add the menubar and shapes 
 		//		borderPane.setTop(menuBar);
 		mainStage.setWidth(primaryScreenBounds.getWidth() * (3.0/4.0));
