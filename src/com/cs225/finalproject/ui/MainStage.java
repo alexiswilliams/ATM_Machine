8c9b3eb840970a3a68cd5f7dedb13b0f13093431
willip27 <pariswilliams@Pariss-MacBook-Pro.local>
2017-06-19 01:44:12 -0400

    Working again with menubar
    menubar login working

--- src/com/cs225/finalproject/ui/MainStage.java
+++ src/com/cs225/finalproject/ui/MainStage.java
@@ -34,7 +34,7 @@ public class MainStage extends Application {
 	getBalanceMenuItem, exitMenuItem, logoutMenuItem, changePinMenuItem,
 	createNewAccountMenuItem, viewTransactionHistoryMenuItem, 
 	contactUsMenuItem, faqMenuItem;
-	
+
 	private Button loginButton, createAccountButton;
 	private GridPane grid;
 	private Label accountNumberLabel, accountPinLabel;
@@ -43,39 +43,45 @@ public class MainStage extends Application {
 	public MainStage() {
 		buildScene(Constants.LOGIN_LABEL);
 	}
-	
+
 	private void updateMenuBar(String sceneType) {
 		// Create MenuBar
-				menuBar = new MenuBar();
-				// Create Menus
-				fileMenu = new Menu(Constants.FILE_LABEL);
-				optionsMenu = new Menu(Constants.OPTIONS_LABEL);
-				helpMenu = new Menu(Constants.HELP_LABEL);
-				// create Menu Items
-				exitMenuItem = new MenuItem(Constants.EXIT_LABEL);
-				contactUsMenuItem = new MenuItem(Constants.CONTACT_US_LABEL);
-				faqMenuItem = new MenuItem(Constants.FAQ_LABEL);
-				if(!sceneType.equals(Constants.LOGIN_LABEL)) {
-					depositMenuItem = new MenuItem(Constants.DEPOSIT_LABEL);
-					withdrawalMenuItem = new MenuItem(Constants.WITHDRAWAL_LABEL);
-					transferFundsMenuItem = new MenuItem(Constants.TRANSFER_FUNDS_LABEL);
-					getBalanceMenuItem = new MenuItem(Constants.GET_BALANCE_LABEL);
-					logoutMenuItem = new MenuItem(Constants.LOGOUT_LABEL);
-					changePinMenuItem = new MenuItem(Constants.CHANGE_PIN_LABEL);
-					createNewAccountMenuItem = new MenuItem(Constants.CREATE_NEW_ACCOUNT_LABEL);
-					viewTransactionHistoryMenuItem = new MenuItem(Constants.VIEW_TRANSACTION_HISTORY_LABEL);
+		menuBar = new MenuBar();
+		// Create Menus
+		fileMenu = new Menu(Constants.FILE_LABEL);
+		optionsMenu = new Menu(Constants.OPTIONS_LABEL);
+		helpMenu = new Menu(Constants.HELP_LABEL);
+		// create Menu Items
+		exitMenuItem = new MenuItem(Constants.EXIT_LABEL);
+		contactUsMenuItem = new MenuItem(Constants.CONTACT_US_LABEL);
+		faqMenuItem = new MenuItem(Constants.FAQ_LABEL);
+		if(!sceneType.equals(Constants.LOGIN_LABEL)) {
+			depositMenuItem = new MenuItem(Constants.DEPOSIT_LABEL);
+			withdrawalMenuItem = new MenuItem(Constants.WITHDRAWAL_LABEL);
+			transferFundsMenuItem = new MenuItem(Constants.TRANSFER_FUNDS_LABEL);
+			getBalanceMenuItem = new MenuItem(Constants.GET_BALANCE_LABEL);
+			logoutMenuItem = new MenuItem(Constants.LOGOUT_LABEL);
+			changePinMenuItem = new MenuItem(Constants.CHANGE_PIN_LABEL);
+			createNewAccountMenuItem = new MenuItem(Constants.CREATE_NEW_ACCOUNT_LABEL);
+			viewTransactionHistoryMenuItem = new MenuItem(Constants.VIEW_TRANSACTION_HISTORY_LABEL);
 
-					// Add menu items to respective menus
-					fileMenu.getItems().addAll(depositMenuItem, withdrawalMenuItem, transferFundsMenuItem, getBalanceMenuItem, exitMenuItem);
-					optionsMenu.getItems().addAll(logoutMenuItem, changePinMenuItem, createNewAccountMenuItem, viewTransactionHistoryMenuItem);
-					helpMenu.getItems().addAll(contactUsMenuItem, faqMenuItem);
-				} else {
-					fileMenu.getItems().add(exitMenuItem);
-					helpMenu.getItems().addAll(contactUsMenuItem, faqMenuItem);
-				}
+			// Add menu items to respective menus
+			fileMenu.getItems().addAll(depositMenuItem, withdrawalMenuItem, transferFundsMenuItem, getBalanceMenuItem, exitMenuItem);
+			optionsMenu.getItems().addAll(logoutMenuItem, changePinMenuItem, createNewAccountMenuItem, viewTransactionHistoryMenuItem);
+			helpMenu.getItems().addAll(contactUsMenuItem, faqMenuItem);
+		} else {
+			fileMenu.getItems().add(exitMenuItem);
+			helpMenu.getItems().addAll(contactUsMenuItem, faqMenuItem);
+		}
 
-				// Add menus to menuBar
-				menuBar.getMenus().addAll(fileMenu, optionsMenu, helpMenu);
+		// Add menus to menuBar
+		menuBar.getMenus().addAll(fileMenu, optionsMenu, helpMenu);
+
+		// disable close button
+		exitMenuItem.setOnAction(e -> {
+			Platform.exit();
+			// TODO add call to generate receipt
+		});
 	}
 
 	private void buildScene(String sceneType) { 
@@ -89,18 +95,18 @@ public class MainStage extends Application {
 			// Create labels for login scene
 			accountNumberLabel = new Label(Constants.ACCOUNT_NUMBER_LABEL);
 			accountPinLabel = new Label(Constants.ACCOUNT_PIN_LABEL);
-			
+
 			// Create inputs for login scene
 			accountNumberInput = new TextField();
 			accountPinInput = new TextField();
 			accountNumberInput.setPromptText(Constants.ACCOUNT_NUMBER_LABEL);
 			accountPinInput.setPromptText(Constants.ACCOUNT_PIN_LABEL);
-			
+
 			grid.setPadding(new Insets(10,10,10,10));
 			// set spacing between nodes
 			grid.setVgap(8);
 			grid.setHgap(10);
-			
+
 			// assign nodes to GridPane
 			GridPane.setConstraints(accountNumberLabel, 0, 0);
 			GridPane.setConstraints(accountNumberInput, 1, 0);
@@ -126,11 +132,6 @@ public class MainStage extends Application {
 		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 
 		mainStage.setResizable(false);
-		// disable close button
-		exitMenuItem.setOnAction(e -> {
-			Platform.exit();
-			// TODO add call to generate receipt
-		});
 		mainStage.setOnCloseRequest(e -> e.consume());
 
 		borderPane = new BorderPane();
@@ -144,7 +145,7 @@ public class MainStage extends Application {
 		mainStage.setScene(scene);
 		mainStage.show();
 	}
-	
+
 	private void updateTopBorder(MenuBar menuBar) {
 		borderPane.setTop(menuBar);
 	}
